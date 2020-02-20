c9bc1a1b58098a96dbb2dfd787d81619aa324955
MikeLudewig <g9731593@campus.ru.ac.za>
2020-02-20 15:48:56 +0200

    *.cs and *.csproj added

--- MyStuff/Problems/Program.cs
+++ MyStuff/Problems/Program.cs
@@ -2,11 +2,25 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        static string Prefix(string input)
+        {
+            int numChar = input.Length;
+            string num = numChar.ToString();
+            
+            int numWords = input.Split().Length;
+            string numW = numWords.ToString();
+            string value = num + "," + numW + ":" + input ;
+            Console.WriteLine(num + "," + numW + ":" + input);
+            return value;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string word = "Hello World!";
+            string sentence = Prefix(word);
+            Console.WriteLine(sentence);
         }
     }
 }
