29345d828504b882208af56280d91968ca7c6b87
MikeLudewig <g9731593@campus.ru.ac.za>
2020-02-23 15:20:19 +0200

    Added all files for storage on remote

--- MyStuff/Problems.Tests/UnitTest1.cs
+++ MyStuff/Problems.Tests/UnitTest1.cs
@@ -12,7 +12,26 @@ namespace Problems.Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            string t1 = Program.Prefix("hello");
+            Assert.AreEqual("5,1:hello", t1);
+            //Assert.Pass();
+        }
+
+         [Test]
+        public void Test2()
+        {
+            string t2 = Program.Prefix("");
+            Assert.AreEqual("0,0:", t2);
+            //Assert.Pass();
+        }
+
+         [Test]
+        public void Test3()
+        {
+            string t3 = Program.Prefix("what      ... did you say??");
+            Assert.AreEqual("27,5:what      ... did you say??", t3);
+            
+            //Assert.Pass();
         }
     }
 }
--- MyStuff/Problems/Program.cs
+++ MyStuff/Problems/Program.cs
@@ -1,18 +1,45 @@
 ﻿using System;
 
+
 namespace Problems
 {
     public class Program
     {
-        static string Prefix(string input)
+        public static string Prefix(string input)
         {
             int numChar = input.Length;
             string num = numChar.ToString();
             
-            int numWords = input.Split().Length;
+            //int numWords = input.Split().Length;
+            string[] inputSplit = input.Split();
+            int numWords = 0;
+            int i = 0;
+
+            while (i < inputSplit.Length)
+            {
+                if (string.IsNullOrWhiteSpace(inputSplit[i]))
+                {
+                    i++;
+                    continue;
+                }
+                else
+                {
+                    i++;
+                    numWords++;
+                }
+                
+            }
+
             string numW = numWords.ToString();
+            
             string value = num + "," + numW + ":" + input ;
-            Console.WriteLine(num + "," + numW + ":" + input);
+
+            //if (string.IsNullOrEmpty(input))
+            //{
+            //    value = num + "," + "0:" + input;
+           //     return value; 
+           // }
+            //Console.WriteLine(num + "," + numW + ":" + input);
             return value;
         }
 
